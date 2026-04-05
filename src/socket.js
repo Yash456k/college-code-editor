24a950701a3e76a6d90982e471fdaf2623d4bf90
yash456k <yash456k@gmail.com>
2026-04-05 21:49:23 +0530

    websocket fix

--- src/socket.js
+++ src/socket.js
@@ -1,11 +1,17 @@
 import { io } from 'socket.io-client';
 
 export const initSocket = async () => {
+    const backendUrl =
+        process.env.REACT_APP_BACKEND_URL ||
+        (window.location.hostname === 'localhost'
+            ? 'http://localhost:5000'
+            : window.location.origin);
+
     const options = {
         'force new connection': true,
         reconnectionAttempts: Infinity,
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io(process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000', options);
+    return io(backendUrl, options);
 };
